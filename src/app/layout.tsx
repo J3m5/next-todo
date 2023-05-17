1ca997d7fb1202748452feafe5e6c3769152e593
JH <je.hu@pm.me>
2023-05-17 09:49:17 +0200

    feat: add Vercel analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,13 +1,17 @@
 import "./globals.css";
 import { Inter } from "next/font/google";
-import styles from "./layout.module.css";
+import { Analytics } from "@vercel/analytics/react";
+// import styles from "./layout.module.css";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
-      <body className="learn-bar">{children}</body>
+      <body className="learn-bar">
+        {children}
+        <Analytics />
+      </body>
     </html>
   );
 }
